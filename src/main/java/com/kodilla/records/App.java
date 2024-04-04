ff740cf82cfd116c6268fdcd0fccf9e540eab254
krzysiek.fit <krzysiek.fit@gmail.com>
2024-04-04 22:40:31 +0200

    Records

---
+++ src/main/java/com/kodilla/records/App.java
@@ -0,0 +1,9 @@
+package com.kodilla.records;
+
+public class App {
+    public static void main(String[] args) {
+        BookRecord book = new BookRecord("John Novak", "Algorithms and other nightmares", 2018);
+        System.out.println(book);
+        int year = book.year();
+    }
+}
